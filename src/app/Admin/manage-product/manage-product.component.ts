f48505ac379dcb28b43ce20bb10d8f6e811d5b49
Nidhi Sahu <nidhinids0902@gmail.com>
2020-08-13 21:59:16 +0530

    ui with console data complete

--- src/app/Admin/manage-product/manage-product.component.ts
+++ src/app/Admin/manage-product/manage-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-product',
@@ -11,5 +12,8 @@ export class ManageProductComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  addPost(formData : NgForm) 
+  {
+      console.log(formData.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -31,6 +32,7 @@ import { ManageProductComponent } from './Admin/manage-product/manage-product.co
     AngularFireAuthModule,
     BrowserAnimationsModule, // required animations module
     NgxUiLoaderModule,
+    FormsModule,
     ToastrModule.forRoot() // ToastrModule added
   ],
   providers: [],
