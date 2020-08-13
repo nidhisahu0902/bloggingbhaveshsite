c36eecc6e35d9fa13c3bbf2a0f479b2ab5bd911c
Nidhi Sahu <nidhinids0902@gmail.com>
2020-08-13 19:47:24 +0530

    dashboard event ui complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './Admin/dashboard/dashboard.component';
+import { ManageProductComponent } from './Admin/manage-product/manage-product.component';
 
 
 const routes: Routes = [
-    {path:'',component:DashboardComponent}
+  { path: '', component: DashboardComponent },
+  {path:'manageproduct',component:ManageProductComponent}
 ];
 
 @NgModule({
