04ea10c87f16d69831d32c158700f79f00dbaa1c
bhaveshgoswami02 <bhaveshgoswami2381998@gmail.com>
2020-08-22 17:30:24 +0530

    chnages

--- src/app/pipes/slug.pipe.ts
+++ src/app/pipes/slug.pipe.ts
@@ -8,5 +8,9 @@ export class SlugPipe implements PipeTransform {
   transform(input: string): string {
     return input.toString().toLowerCase()
       .replace(/\s+/g, '_')           // Replace spaces with  _
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+      .replace(/^-+/, '')             // Trim - from start of text
+      .replace(/-+$/, '');            // Trim - from end of text
   }
 }
